7b966833e738aef49791e526ddcb87906b107a2f
paulesser <hello@paul.esser.com>
2025-11-22 17:31:54 +0100

    update url to quick tunnel

--- raspberry_pi/main.py
+++ raspberry_pi/main.py
@@ -1,10 +1,28 @@
 import time
+from io import BytesIO
 
 # os.environ["PYNPUT_BACKEND_KEYBOARD"] = "uinput"
 import cv2 as cv
+import requests
+from PIL import Image
 from pynput.keyboard import Controller
 
 pressed = True
+
+api = "https://counseling-spectacular-searching-calm.trycloudflare.com "
+
+
+def pil2bytes(image: Image.Image) -> bytes:
+    """
+    converts pil image into bytes
+    Args:
+        image (Image.Image): the source image
+    Returns:
+        bytes: the bytes representation of the image
+    """
+    img_byte_arr = BytesIO()
+    image.save(img_byte_arr, format="PNG")
+    return img_byte_arr.getvalue()
 
 
 def main():
@@ -23,9 +41,17 @@ def main():
                 cv.waitKey(1)
 
                 frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+                pil_image = Image.fromarray(frame_rgb)
+                response = requests.post(
+                    f"{api}/ocr",
+                    files={
+                        "image": pil2bytes(pil_image),
+                    },
+                )
+                print(response.text)
                 if False:
                     keyboard.press("a")
-
+                time.sleep(1)
         except KeyboardInterrupt:
             cam.release()
             cv.destroyAllWindows()
