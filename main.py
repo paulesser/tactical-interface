21192271830b42390e99db906fbafcd31adc7bd7
paulesser <hello@paul.esser.com>
2025-11-22 15:49:56 +0100

    add pynput back

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
-import os
 import time
 
-os.environ["QT_QPA_PLATFORM"] = "xcb"
-
 import easyocr
-import keyboard
 from cv2 import (
-    CAP_PROP_FRAME_HEIGHT,
-    CAP_PROP_FRAME_WIDTH,
-    CAP_V4L2,
     COLOR_BGR2RGB,
     VideoCapture,
     cvtColor,
+    destroyAllWindows,
     imshow,
+    namedWindow,
+    waitKey,
 )
+from pynput.keyboard import Controller
 
 
 def main():
-    cam = VideoCapture(0, CAP_V4L2)
-    cam.set(CAP_PROP_FRAME_WIDTH, 1920)
-    cam.set(CAP_PROP_FRAME_HEIGHT, 1080)
+    cam = VideoCapture(0)
+    # cam.set(CAP_PROP_FRAME_WIDTH, 1920)
+    # cam.set(CAP_PROP_FRAME_HEIGHT, 1080)
     time.sleep(2)
     reader = easyocr.Reader(["de", "en"])
+    keyboard = Controller()
+    namedWindow("Image")
     while True:
         try:
             ret, frame = cam.read()
             if ret:
                 imshow("Captured", frame)
+                waitKey(1)
+
                 frame_rgb = cvtColor(frame, COLOR_BGR2RGB)
+
                 print("frame shape", frame.shape)
                 result = reader.readtext(image=frame_rgb)
                 if len(result) > 0:
                     for r in result:
                         if isinstance(r, tuple):
                             for key in r[1]:
-                                keyboard.write(key)
+                                keyboard.press(key)
                                 print(key)
                         else:
                             print("no keys found")
@@ -45,6 +47,7 @@ def main():
             time.sleep(1)
         except KeyboardInterrupt:
             cam.release()
+            destroyAllWindows()
             print("goodbye!")
             break
 
